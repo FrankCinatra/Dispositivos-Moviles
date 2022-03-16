23ba53ccbdac24f7db7f6bbb5c2ced2eda70f40c
FrankCinatra <L18390031@chetumal.tecnm.mx>
2022-03-16 18:34:05 -0500

    Ejercicio de instalación de plugin de cámara y prueba.

---
+++ CursoCordova/CameraPlugin/platforms/android/app/src/main/assets/www/js/index.js
@@ -0,0 +1,23 @@
+var app = {
+    initialize: function(){
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+    },
+    receivedEvent: function(id){
+        navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI });
+    }
+};
+
+app.initialize();
+
+function onSuccess(imageURI) {
+    var image = document.getElementById('imagen');
+    image.src = imageURI;
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
