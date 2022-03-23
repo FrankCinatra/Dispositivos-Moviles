f53b0b3e8544975530aeb9a91ee94c3f40d97612
FrankCinatra <L18390031@chetumal.tecnm.mx>
2022-03-23 00:24:50 -0500

    Primer app: Hola Kotlin. Ejercicios Sección 2 del curso de Android 12.

---
+++ CursoKotlin/HelloKotlin/app/src/main/java/com/frankcinatra/hellokotlin/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.frankcinatra.hellokotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+    private var tts: TextToSpeech? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        tts = TextToSpeech(this, this)
+        findViewById<Button>(R.id.btnPlay).setOnClickListener { speak() }
+    }
+    private fun speak(){
+        var msj: String = findViewById<TextView>(R.id.etMessage).text.toString()
+        if(msj.isEmpty()){
+            msj = "Por favor, escriba algo"
+        }
+        tts!!.speak(msj, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
+    override fun onInit(status: Int) {
+        if(status==TextToSpeech.SUCCESS){
+            findViewById<TextView>(R.id.tvStatus).text = getString(R.string.tts_active)
+            tts!!.language = Locale("ES")
+        }else findViewById<TextView>(R.id.tvStatus).text = getString(R.string.tts_inactive)
+    }
+    //Mejores practicas
+    //Finalizar servicio despues no usarse
+    override fun onDestroy() {
+        if(tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
+    }
+}
