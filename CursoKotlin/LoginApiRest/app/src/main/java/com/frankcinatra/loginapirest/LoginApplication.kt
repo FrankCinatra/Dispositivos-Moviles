8ed99ce0ec812dbd94c91111cb916abf60dd0a1b
FrankCinatra <L18390031@chetumal.tecnm.mx>
2022-03-26 12:19:59 -0500

    Uso de API REST para proyecto de Login. Segunda mitad de la sección 12, usando una api para ejemplificar el uso de una base de datos para el cual se usa un Login que consulta a esta api y devuelve un mensaje como inicio, registro y perfil de usuario. Ejercicio Sección 12 del curso de Android 12.

---
+++ CursoKotlin/LoginApiRest/app/src/main/java/com/frankcinatra/loginapirest/LoginApplication.kt
@@ -0,0 +1,17 @@
+package com.frankcinatra.loginapirest
+
+import android.app.Application
+
+class LoginApplication : Application() {
+    companion object{
+
+        lateinit var reqResAPI: ReqResAPI
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        //Volley
+        reqResAPI = ReqResAPI.getInstance(this)
+    }
+}
---
+++ CursoKotlin/LoginApiRest/app/src/main/java/com/frankcinatra/loginapirest/Support.kt
@@ -0,0 +1,3 @@
+package com.frankcinatra.loginapirest
+
+data class Support(val url: String, val text: String)
